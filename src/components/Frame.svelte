9cadb37e31e1b1d9ae49b4807a8f3ca927afe18f
Emiliano Ponce <emilianoponce01@gmail.com>
2025-01-24 22:00:08 -0600

    Add Frame component

---
+++ src/components/Frame.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    interface FrameProps {
+        position: 'top' | 'bottom' | 'left' | 'right' | 'corner';
+        color: 'red' | 'purple' | 'blue' | 'brown'
+    }
+
+    export let position: FrameProps['position'] = 'top';
+    export let color: FrameProps['color'] = 'red';
+</script>
+
+<style>
+    .frame {
+        width: 100%;
+    }
+</style>
+
+<div class="frame {position}" style:--background-color="{color}">
+    <slot />
+</div>
