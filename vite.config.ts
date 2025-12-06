e23f8855cd2a968912ae69a232ad23653853a29f
Emiliano Ponce <emilianoponce01@gmail.com>
2025-12-06 13:18:24 -0600

    set up for node instead of stupid cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import tailwind from '@tailwindcss/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite'
+import tailwind from '@tailwindcss/vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-	plugins: [tailwind(),sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+    plugins: [tailwind(), sveltekit()],
+    test: { include: ['src/**/*.{test,spec}.{js,ts}'] },
+})
