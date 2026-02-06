b5e5b7aa3cd120d78cc285632b5d5733ff31f2e1
Emiliano Ponce <emilianoponce01@gmail.com>
2026-02-05 23:22:20 -0600

    remove show enterprise button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
     import { resolve } from '$app/paths'
     import { page } from '$app/stores'
     import Button from '$lib/components/Button.svelte'
-    import Enterprise from '$lib/components/Enterprise.svelte'
     import MediaFilters from '$lib/components/MediaFilters.svelte'
     import ReviewList from '$lib/components/ReviewList.svelte'
     import { onMount } from 'svelte'
@@ -129,19 +128,6 @@
     const hasFilters = $derived(
         data.filters.seriesId || data.filters.seasonId || data.filters.score || data.filters.title
     )
-
-    let showEnterprise = $state(false)
-    let startBlastOff = $state(false)
-    function handleShowEnterprise() {
-        showEnterprise = true
-        setTimeout(() => {
-            startBlastOff = true
-        }, 1000)
-    }
-    function handleEnterpriseReset() {
-        showEnterprise = false
-        startBlastOff = false
-    }
 </script>
 
 <div class="heading">
@@ -149,12 +135,7 @@
     <Button onclick={toggleFilters} sound="beep4">
         {filtersOpen ? 'Hide' : 'Show'} Filters
     </Button>
-    <Button onclick={handleShowEnterprise} sound="beep4">Show Enterprise</Button>
 </div>
-
-{#if showEnterprise}
-    <Enterprise onComplete={handleEnterpriseReset} startBlastOff={startBlastOff} />
-{/if}
 
 <MediaFilters
     bind:this={filterRef}
